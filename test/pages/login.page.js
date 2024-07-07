9d87a2ae5db261888f786b41f2418a9d5309eabb
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-07-07 08:16:07 +0200

    pom

---
+++ test/pages/login.page.js
@@ -0,0 +1,47 @@
+export class LoginPage {
+
+    //locators
+
+    get usernameTextBox() {
+        return $('#username')
+    }
+
+    get usernamePasswordBox() {
+        return $('#password')
+    }
+
+    get loginButton() {
+        return $('button[type=submit]')
+    }
+
+    get checkLogin() {
+        $('#content > div > h4')
+    }
+
+    get isYouLogged() {
+        return $('div[id=flash]')
+    }
+
+    //actions
+
+    async login(username, password) {
+        await this.usernameTextBox.setValue(username);
+        await this.usernamePasswordBox.setValue(password);
+        await this.loginButton.click();
+    }
+
+    async checkLoginAs(message) {
+        await this.checkLogin()
+            .isEqual(message)
+    }
+
+    async isYouLoggedAs(message) {
+        await this.isYouLogged.waitForDisplayed();
+        await expect(isYouLogged)
+            .toHaveText(expect.stringContaining(message));
+    }
+
+
+
+}
+module.exports = new LoginPage();
--- test/specs/myTests/loginTheInternetSite.spec.js
+++ test/specs/myTests/loginTheInternetSite.spec.js
@@ -1,3 +1,5 @@
+const LoginPage = require('@pages/login.page')
+
 describe('Login Test', function (){
 
     it('Login1', async () => {
@@ -6,7 +8,7 @@ describe('Login Test', function (){
         await $('#username').setValue('tomsmith');
         await $('#password').setValue('SuperSecretPassword!');
         await $('button[type=submit]').click();
-        
+
         // assertions
         const checkLogin = $('#content > div > h4');
         await checkLogin.isEqual('Welcome to the Secure Area. When you are done click logout below.');
@@ -14,6 +16,16 @@ describe('Login Test', function (){
         const isYouLogged = await $('div[id=flash]');
         await isYouLogged.waitForDisplayed();
         await expect(isYouLogged).toHaveText(expect.stringContaining('You logged into a secure area!'));
+    })
+
+    it('login POM', async() => {
+
+        browser.url('https://the-internet.herokuapp.com/login')
+
+        await LoginPage.login('tomsmith','SuperSecretPassword!')
+        await LoginPage.checkLoginAs('Welcome to the Secure Area. When you are done click logout below.')
+        await LoginPage.isYouLoggedAs('You logged into a secure area!')
+
     })
     
 })
